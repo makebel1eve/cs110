44515e0dce142ba5b198b150aaf827526fdc4f49
Vishnu Gaddalay <v.gaddalay@iitg.ac.in>
2025-01-28 07:46:22 +0000

    fixed roll number

--- 1.sh
+++ 1.sh
@@ -1,5 +1,5 @@
-mkdir 240
-cd 240
+mkdir 240106066
+cd 240106066
 wget https://raw.githubusercontent.com/vishnugaddalay/cs110/refs/heads/main/1.c -O 1.c
 wget https://raw.githubusercontent.com/vishnugaddalay/cs110/refs/heads/main/2.c -O 2.c
 wget https://raw.githubusercontent.com/vishnugaddalay/cs110/refs/heads/main/3.c -O 3.c
