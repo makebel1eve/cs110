a8755bb122f5861de79727602f33b89ca501e6f2
Vishnu Gaddalay <vishnu@LAPTOP-7P528UHH.>
2025-02-08 12:24:47 +0530

    a

--- 1.sh
+++ 1.sh
@@ -5,6 +5,4 @@ cd $roll
 wget https://raw.githubusercontent.com/vishnugaddalay/cs110/refs/heads/main/1.c -O 1.c
 wget https://raw.githubusercontent.com/vishnugaddalay/cs110/refs/heads/main/2.c -O 2.c
 wget https://raw.githubusercontent.com/vishnugaddalay/cs110/refs/heads/main/3.c -O 3.c
-history -cw
 clear
-
