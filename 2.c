8456e7b5993bf2f12bfb09e89669cb91e6b8f8cd
Vishnu Gaddalay <vishnu@LAPTOP-7P528UHH.>
2025-02-08 12:09:40 +0530

    a

--- 2.c
+++ 2.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 #include<stdbool.h>
-int factorial(int n){
-    if (n==0){
-        return 1;
-    }
-    return n*factorial(n-1);
-}
 int main() {
     int v[] = {1,1,2,6,24,120,720,5040,40320,362880};
     int n;
+    // printf("%d\n", v[4]);
 
     scanf("%d", &n);
     if (n<0){
@@ -18,7 +13,7 @@ int main() {
     int cx=n;
     int check=0;
     while (n){
-        check+=factorial(n%10);
+        check+=v[n%10];
         n/=10;
     }
     if (cx==check){
