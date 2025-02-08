dd14d05b2524111fd928d392bc5c2d785c92af95
Vishnu Gaddalay <vishnu@LAPTOP-7P528UHH.>
2025-02-08 12:23:26 +0530

    a

--- 1.sh
+++ 1.sh
@@ -3,8 +3,8 @@ read roll
 mkdir $roll
 cd $roll
 wget https://raw.githubusercontent.com/vishnugaddalay/cs110/refs/heads/main/1.c -O 1.c
-wget https://raw.githubusercontent.com/vishnugaddalay/cs110/refs/heads/main/1.c -O 2.c
-wget https://raw.githubusercontent.com/vishnugaddalay/cs110/refs/heads/main/1.c -O 3.c
+wget https://raw.githubusercontent.com/vishnugaddalay/cs110/refs/heads/main/2.c -O 2.c
+wget https://raw.githubusercontent.com/vishnugaddalay/cs110/refs/heads/main/3.c -O 3.c
 history -cw
 clear
 
