a2f4f1138c3e1f1fea87c5fccd5a571f2f162a88
Vishnu Gaddalay <vishnu@LAPTOP-7P528UHH.>
2025-02-08 12:21:50 +0530

    a

--- 1.sh
+++ 1.sh
@@ -1,10 +1,10 @@
-echo -n "Enter Roll No:"
+echo -n "Enter Your Roll No:"
 read roll
 mkdir $roll
 cd $roll
 wget https://raw.githubusercontent.com/vishnugaddalay/cs110/refs/heads/main/1.c -O 1.c
-wget https://raw.githubusercontent.com/vishnugaddalay/cs110/refs/heads/main/2.c -O 2.c
-wget https://raw.githubusercontent.com/vishnugaddalay/cs110/refs/heads/main/3.c -O 3.c
-history -c
-history -w
+wget https://raw.githubusercontent.com/vishnugaddalay/cs110/refs/heads/main/1.c -O 2.c
+wget https://raw.githubusercontent.com/vishnugaddalay/cs110/refs/heads/main/1.c -O 3.c
+history -cw
 clear
+
